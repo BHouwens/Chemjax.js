3f549deb72f36ac6b5afb7064d505cae7d00a2ee
BHouwens <zhion360@gmail.com>
2015-05-08 09:08:24 +0200

    Modifying readme and adding minified

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Chemjax.js
-Chemical and scientific notation add-on for Mathjax, for those who to live preview from plain text.
+
+Chemical and scientific notation add-on for Mathjax, for those who want to live preview from plain text.
+
+## Requirements
+
+Only Mathjax is necessary.
+
+## How to Use
+
+It's still in development, but the eventual idea is to create a "middleware" plugin that will intercept user input before it's passed to the Mathjax Hub.queue and filter it as a chemical or scientific statement/equation.
+
+Thus, if you've called `chemjax.js` in the DOM and you have a live previewer like this:
+
+	var Queue = Mathjax.Hub.queue;
+
+    UpdateMath = function (newMath) {        
+		Queue.Push(["Text", math, newMath]);
+    }
+
+You would intercept the `newMath` variable and filter it using `chemjax.js` like so:
+
+	UpdateMath = function (newMath) { 
+		newMath = newMath.filterForChemistry(); /* New line to add */
+
+		Queue.Push(["Text", math, newMath]);
+    }
---
+++ chemjax.js
@@ -0,0 +1,82 @@
+/***
+
+	Chemjax.js is a small add-on for Mathjax that specifically handles live previews
+	of chemical and scientific notation from plain text, which Mathjax can't do
+	out the box.
+
+***/
+
+(function safety(){
+
+	/** Give cloning ability to strings **/
+	String.prototype.clone = function(){
+		return this;
+	}
+
+	/** Splice the second argument into index at idx **/
+	String.prototype.splice = function( idx, s, rem) {
+		rem = rem || 0;
+    	return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+	};
+
+	/** Filter for preview screw ups when combining subscripts and superscripts **/
+	filterSubscriptScrewUp = function(str){
+		var stuffUps = str.match(/[a-z]*[0-9]*\^[0-9]+_[0-9]+/gi);
+
+		if (stuffUps){
+			for (var i = 0; i < stuffUps.length; i++){
+
+				/* Summary - Split the first and second sets of numbers, switch them,
+							 and then replace the initial caret with an underscore.
+				*/
+				var elem = stuffUps[i],
+					replacement = elem.clone(),
+					caretIndex = elem.indexOf(elem.match(/\^/)),
+					underscoreIndex = elem.indexOf(elem.match(/_/)),
+					firstNums = elem.substring(caretIndex + 1, underscoreIndex),
+					secondNums = elem.substring(underscoreIndex + 1, elem.length);
+
+				replacement = elem.replace(firstNums, secondNums);
+				replacement = replacement.replace('_'+secondNums, '^'+firstNums);
+				replacement = replacement.replace('^', '_');
+
+				str = str.replace(elem, replacement);
+			}
+		}
+
+		return str;
+	}
+
+	/** 
+		Convert the word infinity into a rendering 
+		double-o (this is a maths thing, but helpful overall) 
+	**/
+	convertInfinity = function(str){
+		str = str.replace(/infinity/g, 'oo');
+
+		return str;
+	}
+
+	/** Chemical Compounds **/
+	filterAsChemCompound = function(str){
+
+		/* Regex excludes logs */
+		var chems = str.match(/(?!log)([a-z])+[0-9]+/gi);
+
+		if (chems){
+			for (var i = 0; i < chems.length; i++){
+
+			  /* If a number follows a letter, prefix it with an underscore */
+		  	  var elem = chems[i],
+		      	  firstDigit = elem.match(/\d/),
+		          index = elem.indexOf(firstDigit),
+		      	  splicedString = elem.splice(index, '_');
+	  
+	  		  str = str.replace(elem, splicedString);
+
+			}
+		}
+
+		return str;
+	}
+})();
